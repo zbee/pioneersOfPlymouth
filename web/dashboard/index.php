2d8734d02d717bd87e2adf941193ada9baf49c91
Ethan Henderson <ethan@zbee.me>
2018-09-09 00:34:39 -0600

    Dashboard now complete ... with clickable tabs for data
    Signed-off-by: Ethan Henderson <ethan@zbee.me>

--- web/dashboard/index.php
+++ web/dashboard/index.php
@@ -1,2 +1,77 @@
 <?php
 require_once '/var/www/pop/web/assets/autoload.php';
+
+if (!$isLoggedIn)
+  $UserSystem->redirect301('/user/login?mustBeLoggedIn')
+?>
+
+<div class="ribbon">
+  <div class="mainContent">
+    <span class="largestText">Welcome In</span>
+
+    <div class="tabHolder">
+
+      <a class="tab" href="/">
+        <span class="header">
+          Game Browser
+        </span>
+        <div class="content">
+          <div>
+            131
+          </div>
+          <div>
+            Games Open
+          </div>
+        </div>
+      </a>
+
+      <div class="deadSpace"></div>
+
+      <a class="tab">
+        <span class="header">
+          Statistics
+        </span>
+        <div class="content">
+          <div>
+            S
+          </div>
+          <div>
+            Skill Rank
+          </div>
+        </div>
+      </a>
+
+      <a class="tab">
+        <span class="header">
+          Friends
+        </span>
+        <div class="content">
+          <div>
+            131
+          </div>
+          <div>
+            Games Open
+          </div>
+        </div>
+      </a>
+
+      <div class="deadSpace"></div>
+
+      <a class="tab">
+        <span class="header">
+          Account
+        </span>
+        <div class="content">
+          <div>
+            S
+          </div>
+          <div>
+            Skill Rank
+          </div>
+        </div>
+      </a>
+
+    </div>
+
+  </div>
+</div>
