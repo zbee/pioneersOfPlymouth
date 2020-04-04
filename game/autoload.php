76efd0919154230d31a673c861ac9b24ff519ccf
Ethan Henderson <ethan@zbee.codes>
2020-04-04 01:37:03 -0600

    Start basic game files
    Signed-off-by: Ethan Henderson <ethan@zbee.me>

--- game/autoload.php
+++ game/autoload.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
-require_once 'core/core.php';
-require_once 'play/play.php';
-require_once 'feedAjax/feedAjax.php';
+require_once 'config.inc';
+require_once 'core/core.inc';
+require_once 'play/play.inc';
+require_once 'connection/ajaxFeed/ajaxFeed.inc';
