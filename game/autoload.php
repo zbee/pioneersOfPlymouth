b7cdcaf699756d53cbd1da58abec8739c767ba27
Ethan Henderson <ethan@zbee.codes>
2020-04-07 23:12:01 -0600

    Document loader
    Signed-off-by: Ethan Henderson <ethan@zbee.me>

--- game/autoload.php
+++ game/autoload.php
@@ -1,8 +1,14 @@
 <?php
+//Include composer packages
 require_once 'vendor/autoload.php';
+
+//Configuration for POP
 require_once 'config.inc';
+
+//Include game logic
 require_once 'core/core.inc';
 require_once 'play/play.inc';
 require_once 'connection/ajaxFeed/ajaxFeed.inc';
 
+//Generate the base POP object
 $pop = new pioneersOfPlymouth();
