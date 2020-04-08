de4b05404ad9c85672589645b801248f387b5f50
Ethan Henderson <ethan@zbee.codes>
2020-04-07 22:57:54 -0600

    Add database abstraction layer
    Signed-off-by: Ethan Henderson <ethan@zbee.me>

--- game/autoload.php
+++ game/autoload.php
@@ -1,5 +1,8 @@
 <?php
+require_once 'vendor/autoload.php';
 require_once 'config.inc';
 require_once 'core/core.inc';
 require_once 'play/play.inc';
 require_once 'connection/ajaxFeed/ajaxFeed.inc';
+
+$pop = new pioneersOfPlymouth();
