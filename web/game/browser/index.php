7e58211284e0a64bad42892bb5229c6a660dc906
Ethan Henderson <ethan@zbee.codes>
2020-04-08 00:36:30 -0600

    Build and implement browser logic class
    Signed-off-by: Ethan Henderson <ethan@zbee.me>

--- web/game/browser/index.php
+++ web/game/browser/index.php
@@ -4,16 +4,8 @@ require_once '/var/www/pop/web/assets/autoload.php';
 if (!$isLoggedIn)
   $UserSystem->redirect('/user/login?mustBeLoggedIn');
 
-$currentTimestamp = (new DateTime())->getTimestamp();
-$ageRestriction = $currentTimestamp - (60 * 60 * 2);
-
-$games = $UserSystem->dbSel(
-  [
-    'gameLobbies',
-    ['date' => ['>', $ageRestriction]],
-    ['name', 'asc']
-  ]
-);
+$browser = new browse($pop);
+$games = $browser->browser;
 
 echo '<div class="ribbon"><div><table><tr>';
 echo '<th></th>';
