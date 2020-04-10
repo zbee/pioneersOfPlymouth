3585f4079a9cbabc34d7de849d7330280a7c9a63
Ethan Henderson <ethan@zbee.codes>
2020-04-10 10:51:48 -0600

    Implement player list in lobby
    Signed-off-by: Ethan Henderson <ethan@zbee.me>

--- web/game/lobby.php
+++ web/game/lobby.php
@@ -15,9 +15,45 @@ $pop->loadLobby($lobbyID);
 
 if ($pop->lobbyLoaded !== true)
   $UserSystem->redirect('/dashboard?gameNonexistent');
+$lobby = $pop->getLobby();
+$lobby->loadPlayers($pop);
 ?>
 
-<div class="ribbon balance">
-  <div>content</div>
-  <div>not content</div>
+<div class="ribbon balance" id="title">
+  <div class="mainContent"><h2><?= $lobby->name ?></h2></div>
+  <div>by <a href="#"><?= $lobby->owner->username ?></a></div>
 </div>
+<div class="ribbon evenLeft small" id="basic_info">
+  <div>
+    Invite Only: <?= $lobby->inviteOnlyText ?>
+    &nbsp; &nbsp; - &nbsp; &nbsp;
+    Language: <?= $lobby->language ?>
+  </div>
+  <div>
+    Lobby ID:
+    <small><?= $lobby->uuid ?></small>
+    <sup><a onclick="copyToClipboard('<?= $lobby->uuid ?>')">(copy)</a></sup>
+  </div>
+</div>
+<div class="ribbon even" id="players_settings">
+  <div>
+    <?php foreach ($lobby->players as $player): ?>
+      <?= $player->username ?><br>
+    <?php endforeach; ?>
+  </div>
+  <div class="mainContent"></div>
+</div>
+
+<script>
+  const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+</script>
