797eabbcbd2d6ba504f70b7678819ac3a273e97e
Ethan Henderson <ethan@zbee.codes>
2020-04-09 22:19:54 -0600

    Provide installation instructions
    Signed-off-by: Ethan Henderson <ethan@zbee.me>

---
+++ INSTALL.md
@@ -0,0 +1,80 @@
+#Out of the box install
+Here are the basic setup items which all need to be done, which is probably
+exactly what you need if you already have an Ubuntu PHP box, which you need to
+upgrade to PHP 7.4, install missing extensions, and get services up. The only
+dependencies for Pioneers of Plymouth are some php extensions, stylus
+(which requires npm), composer, one composer package which is DBAL
+(this is set to a specific version in the composer file), and zbee/UserSystem,
+which is included and modified.
+
+Pioneers of Plymouth is written for PHP 7.4. If you do not have PHP 7.4
+installed then you can follow
+[this guide](https://computingforgeeks.com/how-to-install-php-on-ubuntu/)
+or here are some highlight commands:
+
+```
+$ sudo apt-get update
+$ sudo apt install software-properties-common
+$ sudo add-apt-repository ppa:ondrej/php
+$ sudo apt-get update
+$ sudo apt install php7.4 libapache2-mod-php7.4
+$ sudo apt install php7.4-{bcmath,bz2,cgi,cli,common,dba,dev,enchant,fpm,gd,gmp,imap,interbase,intl,json,ldap,mbstring,odbc,opcache,pgsql,phpdbg,pspell,readline,snmp,soap,sqlite3,sybase,tidy,xml,xmlrpc,xsl,zip,memcached,mysql}
+$ sudo a2enmod php7.4
+```
+
+If you already had PHP then you'll need to disable your other version, replace
+the Xs with what your version was:
+
+```
+$ sudo a2dismod phpX.X
+```
+
+You will probably also want to copy your old `php.ini` over to 7.4 if you
+already had another version of PHP installed:
+
+```
+$ sudo cp /etc/php/X.X/apache2/php.ini /etc/php/7.4/apache2/php.ini
+```
+
+The additional php7.4-[packages] are what I needed to run Pioneers of Plymouth
+as well as other software - you could reduce this list, but this will definitely 
+give you a solution.
+
+The next installation you'll need is memcache, which can be installed similarly:
+
+```
+$ sudo apt install php7.4-memcache
+$ sudo a2enmod php7.4-memcache
+$ sudo service start memcached
+```
+
+The first thing configuration you'll need to update is for allowing .htaccess
+files. Add this to the apache2 config for your directory: 
+
+```
+<Directory /path/to/pioneersOfPlymouth>
+  Options Indexes FollowSymLinks
+  AllowOverride All
+  Require all granted
+</Directory>
+```
+
+Next you'll want to be sure to compile the stylus included for styling. Navigate
+to the `/web/assets/stylus` folder and run the following:
+
+```
+$ npm install stylus -g
+$ stylus main.styl -o ../css/main.css -c
+```
+
+Then you'll want to install the [composer](https://getcomposer.org/download/)
+packages in the `/game/` directory:
+```
+$ composer install
+```
+
+When you setup your Pioneers of Plymouth database in your MySQL
+database, you can use [the DDL](database_setup.sql) to generate the
+tables. However, make sure the default engine is set to InnoDB, or alter
+the DDL to set each table to be InnoDB manually.
+This is required as foreign key constraints are in use.
---
+++ database_setup.sql
@@ -0,0 +1,106 @@
+create schema pop collate latin1_swedish_ci;
+
+create table ban
+(
+    id int(20) auto_increment
+        primary key,
+    date int(20) null,
+    ip varchar(60) null,
+    user binary(16) null,
+    issuer int default 0 not null,
+    reason varchar(512) default 'No reason provided.' not null,
+    appealed int(1) default 0 not null
+);
+
+create table connections
+(
+    userID binary(16) not null comment 'The connected user',
+    connectionType smallint(1) default 0 not null comment 'The type of connection that is open; 0=site, 1=browser, 2=lobby, 3=game, 4=post-game',
+    connectionID binary(16) null,
+    lastPing int not null,
+    constraint connections_userID_uindex
+        unique (userID)
+)
+    comment 'Table containing users connected to games and lobbies';
+
+alter table connections
+    add primary key (userID);
+
+create table gameLobbies
+(
+    lobbyID binary(16) not null comment 'Binary UUID of the game'
+        primary key,
+    name varchar(40) not null comment 'User-provided name for the game lobby',
+    inviteOnly tinyint default 0 not null comment 'Whether or not the game is invite-only',
+    maxPlayers tinyint default 3 not null comment 'Number of players allowed to join; must be greater than 3',
+    expansionsIncluded tinyint default 0 not null comment 'Identifier of expansions that are used in this game; raises the maxPlayers possibility',
+    mapSetup int(57) unsigned not null,
+    date int not null comment 'Unix timestamp game was created',
+    owner binary(16) not null comment 'Link to the user UUID that created the game lobby',
+    language varchar(2) default 'en' not null comment 'ISO 639-1 code of the language the owner speaks',
+    constraint gameLobbies_ak_1
+        unique (owner)
+)
+    comment 'List of games actively being played or open for joining from the browser';
+
+create index gameLobbies_idx_1
+    on gameLobbies (name);
+
+create index gameLobbies_idx_2
+    on gameLobbies (inviteOnly);
+
+create index gameLobbies_idx_3
+    on gameLobbies (expansionsIncluded);
+
+create index gameLobbies_idx_4
+    on gameLobbies (date);
+
+create index gameLobbies_idx_5
+    on gameLobbies (owner);
+
+create index gameLobbies_idx_6
+    on gameLobbies (language);
+
+create index gameLobbies_idx_7
+    on gameLobbies (lobbyID, owner);
+
+create table userblobs
+(
+    id int(20) auto_increment
+        primary key,
+    user binary(16) not null,
+    code varchar(160) not null,
+    action varchar(20) not null,
+    date int(20) not null
+);
+
+create table users
+(
+    id binary(16) not null
+        primary key,
+    username varchar(50) not null,
+    firstName varchar(256) null,
+    lastName varchar(256) null,
+    password varchar(100) not null,
+    oldPassword varchar(100) null,
+    passwordChanged varchar(20) default '0000000000' not null,
+    salt varchar(512) not null,
+    email varchar(256) not null,
+    oldEmail varchar(512) null,
+    emailChanged int(2) default 0 not null,
+    phone int default 0 not null,
+    dateRegistered int(20) not null,
+    activated int(1) default 0 not null,
+    title varchar(50) default '' not null,
+    twoStep int(1) default 0 not null,
+    lastLoggedIn int(20) default 0 not null,
+    oldLastLoggedIn int(20) default 0 not null,
+    ip varchar(60) default '' not null
+);
+
+create definer = admin@localhost trigger before_insert_uuid_the_user
+    before insert
+    on users
+    for each row
+    SET new.id = unhex(replace(uuid(),'-',''));
+
